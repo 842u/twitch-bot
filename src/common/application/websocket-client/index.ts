c488d216031838b818e4f85758c6efced75bae2a
Kamil Bazanow <kamil.bazanow@gmail.com>
2025-11-19 09:49:41 +0100

    refactor(WebSocketClient): use interface instead of type

--- src/common/application/websocket-client/index.ts
+++ src/common/application/websocket-client/index.ts
@@ -1,6 +1,6 @@
 type WebScoketEvent = "open" | "message" | "error" | "close";
 
-export type WebSocketClient = {
+export interface WebSocketClient {
 	on(event: WebScoketEvent, callback: (event: Event) => void): void;
 	send(data: string): void;
-};
+}
