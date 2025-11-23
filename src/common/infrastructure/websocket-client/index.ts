61e9b93d3ad0b46de7c33cc422db3542ffa65b5b
Kamil Bazanow <kamil.bazanow@gmail.com>
2025-11-23 15:51:36 +0100

    refactor(DefaultWebSocketClient): remove keepAlive()

--- src/common/infrastructure/websocket-client/index.ts
+++ src/common/infrastructure/websocket-client/index.ts
@@ -1,45 +1,10 @@
 import type { WebSocketClient } from "@/common/application/websocket-client/index.js";
 
-type Options = {
-	keepAliveIntervalMiliseconds: number;
-};
-
-const defaultOptions = {
-	keepAliveIntervalMiliseconds: 1000 * 60 * 2,
-};
-
 export class DefaultWebScoketClient implements WebSocketClient {
 	private socket: WebSocket;
 
-	private keepAliveIntervalId: NodeJS.Timeout | undefined;
-	private keepAliveIntervalMiliseconds: number;
-	private keepAlivePingCounter: number = 0;
-
-	constructor(url: string | URL, options: Options = defaultOptions) {
+	constructor(url: string | URL) {
 		this.socket = new WebSocket(url);
-		this.keepAliveIntervalMiliseconds = options.keepAliveIntervalMiliseconds;
-
-		if (this.keepAliveIntervalMiliseconds) {
-			this.keepAlive(this.keepAliveIntervalMiliseconds);
-		}
-	}
-
-	private keepAlive(keepAliveIntervalMiliseconds: number) {
-		this.on("open", () => {
-			this.keepAliveIntervalId = setInterval(() => {
-				this.keepAlivePingCounter++;
-				this.socket.send("ping");
-			}, keepAliveIntervalMiliseconds);
-		});
-
-		this.on("close", () => clearInterval(this.keepAliveIntervalId));
-	}
-
-	get aliveTime() {
-		const aproxTimeMiliseconds = this.keepAlivePingCounter * this.keepAliveIntervalMiliseconds;
-		const aproxTimeSeconds = aproxTimeMiliseconds / 1000;
-		console.log(`Connection is alive for about: $${aproxTimeSeconds} s.`);
-		return aproxTimeSeconds;
 	}
 
 	on(event: "open", callback: (event: Event) => void): void;
